bdadd4cc001c5f33a45ee2051f32a59a47ea9af6
Shritej Mayekar <30909435+shritejmayekar@users.noreply.github.com>
2022-12-05 23:06:11 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -18,7 +18,7 @@ function Header({text,bgColor,textColor}) {
 
 Header.defaultProps = {
     text:'FeedBack UI',
-    bgColor:'rgba(0,0,0,0.4)',
+    bgColor:'#000000e6',
     textColor:'#ff6a95'
 }
 Header.propTypes = {
